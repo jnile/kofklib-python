e7201aa8d260748a3cf42ebe984f1fbcad48f5c0
jnile <jaidevnileshkumar2002@gmail.com>
2024-02-12 13:33:52 +0000

    Refine Matrix constructor

--- Matrices/Matrix.py
+++ Matrices/Matrix.py
@@ -1,17 +1,38 @@
+import collections.abc
+
 class Matrix(object):
     matrix = []
     
+    #Constructor
+    #
+    # Defines a 2x2 Matrix
+    def __init__(self, arr,):
+        self.matrix = arr
+        
+        if type(arr[0]) is list:
+            self.row = len(arr)
+            self.columns = len(arr[0])
+        else:
+            self.matrix = [arr]
+            self.row = 1
+            self.columns = len(arr)
     
-    def __init__(self, rows, columns):
-        self.matrix = [[0 for j in range(columns)] for i in range(rows)]
-        self.rows = rows  
-        self.columns = columns  
+    @classmethod
+    def blank_matrix(cls, rows, columns):
+        temp_arr = [[0] * columns for i in range(rows)]
+        return cls(temp_arr)
     
+    
+    
+    #PrintMatrix
+    #
+    #Prints the matrix in terminal
     def printMatrix(self):
         print("Matrix:\n")
         
         for eachRow in self.matrix:
             row = ""
+            
             for eachCol in eachRow:
                 row += str(eachCol) + " "
             
@@ -19,3 +40,10 @@ class Matrix(object):
             print(row)
             
         print("\n")
+    
+    #getMatrix
+    #
+    # Return the matrix as an array
+    def getMatrix(self):
+        return [[y for y in x] for x in self.matrix]
+        
