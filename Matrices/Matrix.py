84413687bee578d087650adddadf54b06a70781d
jnile <jaidevnileshkumar2002@gmail.com>
2024-02-12 13:04:13 +0000

    Add print matrix function

--- Matrices/Matrix.py
+++ Matrices/Matrix.py
@@ -1,6 +1,21 @@
 class Matrix(object):
     matrix = []
     
+    
     def __init__(self, rows, columns):
-        self.matrix = [[0 for j in range(columns)] for i in range(rows)]   
+        self.matrix = [[0 for j in range(columns)] for i in range(rows)]
+        self.rows = rows  
+        self.columns = columns  
+    
+    def printMatrix(self):
+        print("Matrix:\n")
+        
+        for eachRow in self.matrix:
+            row = ""
+            for eachCol in eachRow:
+                row += str(eachCol) + " "
             
+            row += "\n"
+            print(row)
+            
+        print("\n")
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # KOFKLIB
-## Python Edition
+## Python Edition (Python 3.11.6)
 
 This is a repository/library storing useful modules and experiments created by me.
